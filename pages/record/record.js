8f9072685060954d9ce2a5134d5955199405be99
xbaobaox <xbaobaox@sina.com>
2019-11-21 22:48:37 +0800

    Initial Commit

---
+++ pages/record/record.js
@@ -0,0 +1,28 @@
+// pages/record/record.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    data:'2019-11-01'
+  },
+  bindDateChange(e){
+    this.setData({
+      data:e.detail.value
+    })
+  },
+  onLoad:function(){
+    wx.request({
+      url: 'https://www.shuipingguo.com/interview/get_qrecord',
+      method: 'get',
+      header: {
+        'content-type':'application/json'
+      },
+      success:(res) => {
+        console.log(res.data);
+      }
+    })
+  }
+  
+})
