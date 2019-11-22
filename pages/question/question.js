feaef8c23d7778236045fd733a690412cfc3fa77
xbaobaox <xbaobaox@sina.com>
2019-11-22 22:23:08 +0800

    11.22 22.22

--- pages/question/question.js
+++ pages/question/question.js
@@ -5,70 +5,63 @@ Page({
    * 页面的初始数据
    */
   data: {
-
-  },
-  data: {
-    data: '2019-11-01',
-    fuck: 0
+    data: '2019-11',
+    subject: []
   },
   bindDateChange(e) {
     this.setData({
       data: e.detail.value
+    }),
+      wx.request({
+        url: 'https://www.shuipingguo.com/interview/get_qrecord',
+        method: 'get',
+        data: {
+          month: this.data.data
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded',
+          "cookie": "oid=oO_UR0ZNNuoD-11Mk1QWcvkO5X7g"
+        },
+        success: (res) => {
+          this.setData({
+            subject: res.data.data
+          })
+          // console.log(this.data.data)
+          console.log(res.data.data.length);
+        },
+        fail: function (e) {
+
+        },
+        complete(e) {
+
+        }
+      })
+  },
+  onLoad: function () {
+    wx.request({
+      url: 'https://www.shuipingguo.com/interview/get_qrecord',
+      method: 'get',
+      data: {
+        month: "2019-10"
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+        "cookie": "oid=oO_UR0ZNNuoD-11Mk1QWcvkO5X7g"
+      },
+      success: (res) => {
+        this.setData({
+          subject: res.data.data
+        })
+
+        console.log(res.data.data);
+      },
+      fail: function (e) {
+
+      },
+      complete(e) {
+
+      }
     })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
+
 })
--- pages/record/record.js
+++ pages/record/record.js
@@ -5,35 +5,77 @@ Page({
    * 页面的初始数据
    */
   data: {
-    data: '2019-11-01',
-    fuck:0
+    data: '2019-11',
+    subject: [],
+    dialogShow: false,
+    confirm: [{
+      text: '确定'
+    }]
   },
-  bindDateChange(e){
+  openConfirm() {
     this.setData({
-      data:e.detail.value
+      dialogShow: true
     })
   },
-  onLoad:function(){
+  tapDialogButton(){
+    this.setData({
+      dialogShow:false
+    })
+  },
+  bindDateChange(e) {
+    this.setData({
+        data: e.detail.value
+      }),
+      wx.request({
+        url: 'https://www.shuipingguo.com/interview/get_qrecord',
+        method: 'get',
+        data: {
+          month: this.data.data
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded',
+          "cookie": "oid=oO_UR0ZNNuoD-11Mk1QWcvkO5X7g"
+        },
+        success: (res) => {
+          this.setData({
+            subject: res.data.data
+          })
+          // console.log(this.data.data)
+          console.log(res.data.data.length);
+        },
+        fail: function(e) {
+
+        },
+        complete(e) {
+
+        }
+      })
+  },
+  onLoad: function() {
     wx.request({
       url: 'https://www.shuipingguo.com/interview/get_qrecord',
       method: 'get',
+      data: {
+        month: "2019-10"
+      },
       header: {
-        // 'content-type':'application/json'
+        'content-type': 'application/x-www-form-urlencoded',
+        "cookie": "oid=oO_UR0ZNNuoD-11Mk1QWcvkO5X7g"
       },
       success: (res) => {
         this.setData({
-          fuck:res.data.retcode
+          subject: res.data.data
         })
-        
-        console.log(res);
+
+        console.log(res.data.data);
       },
-      fail: function (e) {
-        
+      fail: function(e) {
+
       },
       complete(e) {
-        
+
       }
     })
   }
-  
+
 })
