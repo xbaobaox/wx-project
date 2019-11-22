76e9001522c1945eb06770b35a0e6ca68ab0be5b
xbaobaox <xbaobaox@sina.com>
2019-11-22 14:45:41 +0800

    11.22 14.45

--- pages/record/record.js
+++ pages/record/record.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    data:'2019-11-01'
+    data: '2019-11-01',
+    fuck:0
   },
   bindDateChange(e){
     this.setData({
@@ -17,11 +18,20 @@ Page({
       url: 'https://www.shuipingguo.com/interview/get_qrecord',
       method: 'get',
       header: {
-        'content-type':'application/json'
+        // 'content-type':'application/json'
       },
       success: (res) => {
+        this.setData({
+          fuck:res.data.retcode
+        })
         
-        console.log(res.data);
+        console.log(res);
+      },
+      fail: function (e) {
+        
+      },
+      complete(e) {
+        
       }
     })
   }
